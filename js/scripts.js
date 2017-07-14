509dd39d4cebbd0883b80a53586f3a3bffd0a95f
Kate Byars <katebyars5@gmail.com>
2017-07-14 15:09:16 -0700

    add final edits to csc, index and js.

--- js/scripts.js
+++ js/scripts.js
@@ -11,28 +11,29 @@ $(document).ready(function() {
 
 ///Back End Logic///
 
-  function returnCountDown(anArray, userInput) {
-    var aNumber = parseInt(userInput);
-    anArray.push(aNumber);
-      for (var i = aNumber; aNumber > 1; i += 1) {
-          aNumber -= 1 ;
-          anArray.push(aNumber);
-      }
-      anArray.reverse();
-      return anArray;
+function returnCountDown(anArray, userInput) {
+  var aNumber = parseInt(userInput);
+  anArray.push(aNumber);
+
+  for (var i = aNumber; aNumber > 1; i += 1) {
+      aNumber -= 1 ;
+      anArray.push(aNumber);
   }
+  anArray.reverse();
+  return anArray;
+}
 
-  function getResult(anArray, userInput) {
-    var theInput = parseInt(userInput);
+function getResult(anArray, userInput) {
+  var theInput = parseInt(userInput);
 
-      if (theInput%3===0 && theInput%5!=0) {
-        return "ping";
-      } else if (theInput%5===0 && theInput%15===0) {
-       return "ping-pong";
-     } else if (theInput%5===0) {
-       return "pong";
-     } else if ((theInput%3!=0)||(theInput%5!=0)) {
-       returnCountDown(anArray, userInput);
-       return anArray;
-     }
+  if (theInput%3===0 && theInput%5!=0) {
+      return "ping";
+  } else if (theInput%5===0 && theInput%15===0) {
+     return "ping-pong";
+  } else if (theInput%5===0) {
+     return "pong";
+  } else if ((theInput%3!=0)||(theInput%5!=0)) {
+     returnCountDown(anArray, userInput);
+     return anArray;
+  }
 }
