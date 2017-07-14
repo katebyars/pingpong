93b8f833ac8ed02343976c249813782300bf110a
Kate Byars <katebyars5@gmail.com>
2017-07-14 11:59:53 -0700

    return for arrays counting to any number.

--- js/scripts.js
+++ js/scripts.js
@@ -3,30 +3,43 @@ $(document).ready(function() {
   $("form#userInput").submit(function(event) {
     var input = $("input:text").val();
     var newArray = [];
-
-    var result = returnCountDown(newArray, input);
-
-    $("#result").append((result.reverse()) + "<br>");
+    var result = getResult(newArray, input);
+    $("#result").append(result + "<br>");
     event.preventDefault();
   });
 });
 
 ///Back End Logic///
 
-
-
+  // function getResult (userInput, anArray) {
+  //   if (userInput != 3 ) {
+  //
+  //   returnCountDown(anArray, userInput);
+  //   }
+  // }
+  //
   function returnCountDown(anArray, userInput) {
+    var aNumber = parseInt(userInput);
+    anArray.push(aNumber);
+      for (var i = aNumber; aNumber > 1; i += 1) {
+          aNumber -= 1 ;
+          anArray.push(aNumber);
+      }
+      anArray.reverse();
+      return anArray;
+  }
 
-    if (userInput = "3") {
-      return "ping";
-    } else if (userInput = "5") {
-      return "pong";
-    } else {
-        var aNumber = parseInt(userinput);
-        anArray.push(aNumber);
-          for (var i = aNumber; aNumber > 1; i += 1) {
-              aNumber -= 1 ;
-              anArray.push(aNumber);
-          }
-          return anArray;
-        }
+  function getResult(anArray, userInput) {
+    // alert("hi");
+    // debugger;
+      if ((userInput!="3") && (userInput!="5") && (userInput!="15")) {
+        returnCountDown(anArray, userInput);
+        return anArray;
+      } else if (userInput==="3") {
+        return "ping";
+      } else if (userInput==="5") {
+       return "pong";
+     } else if (userInput==="15") {
+       return "ping-pong";
+     }
+}
